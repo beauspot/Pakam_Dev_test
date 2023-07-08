3b18bea6473d531ccb19a3bdee64892289e7ba5e
beauspot <beauspot@outlook.com>
2023-07-08 19:23:51 +0100

    corrected the errorHandlerMiddleware.

--- src/api/middleware/errHandlerMiddleware.js
+++ src/api/middleware/errHandlerMiddleware.js
@@ -2,9 +2,8 @@ const { StatusCodes } = require("http-status-codes");
 
 // Error handling middleware
 const errMiddleware = (err, req, res, next) => {
-  console.error("Error: ", err.message);
-  if (err)
-    return res.status(err.statusCode).json({ erroorMessage: err.message });
+  console.error("Error:", err.message);
+  if (err) return res.status(404).json({ errorMessage: err.message });
   return res
     .status(StatusCodes.INTERNAL_SERVER_ERROR)
     .json({ errorMessage: err.message });
