bc8fe26812973bc0f73373a96bb926bfabae84c1
beauspot <beauspot@outlook.com>
2023-07-08 09:58:55 +0100

    the entery point of the application.

--- src/api/server.js
+++ src/api/server.js
@@ -4,6 +4,14 @@ require("express-async-errors");
 
 const server = express();
 
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+// importing the routes
+const notificationRoute = require("./routes/appRoute");
+
+server.use("/api/v1/notifyuser", notificationRoute);
+
 const port = 4130;
 
 server.listen(port, (err) => {
