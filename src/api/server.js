6dc48d16eb997e797ddc984a0f476cf0e2efde73
beauspot <beauspot@outlook.com>
2023-07-09 10:08:13 +0100

    modified the server.js file

--- src/api/server.js
+++ src/api/server.js
@@ -1,7 +1,10 @@
-//require("express-async-errors");
+require("express-async-errors");
 const express = require("express");
 const morgan = require("morgan");
 const { StatusCodes } = require("http-status-codes");
+
+// DB configurations
+const dbConfig = require("../api/config/dbConfig");
 
 // Error Middlware
 const errMidd = require("./middleware/errHandlerMiddleware");
@@ -25,7 +28,17 @@ server.use("/api/v1/notifyuser", notificationRoute);
 server.use(errMidd.errMiddleware);
 const port = 4130;
 
-server.listen(port, (err) => {
-  if (!err) console.info("Server started on port " + port);
-  else console.error("Couldn't listen on port " + port);
-});
+const startServer = async () => {
+  try {
+    await dbConfig(process.env.MONGO_URL);
+    server.listen(port, (err) => {
+      if (!err) console.info("Server started on port " + port);
+      else console.error("Couldn't listen on port " + port);
+    });
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
