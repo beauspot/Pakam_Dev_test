2e5e11cf269cd77fbb8d0d6d22e3d8e27c541542
beauspot <beauspot@outlook.com>
2023-07-09 10:08:54 +0100

    the user services module

--- src/api/services/userService.js
+++ src/api/services/userService.js
@@ -1,15 +1,12 @@
-const generateDummyUsers = require("../dummyData");
-// userService.js
 
-let users = generateDummyUsers();
-console.log(users);
+const userModel = require("../models/userModel");
+
+/* let users = generateDummyUsers();
+console.log(users); */
 
 // Fetch user information from User Management Service
 const fetchUserInformation = async (userId) => {
-  // Assume an API endpoint '/users/:userId' that retrieves user information
-  const user = users.find(
-    (user) => user.id === userId || user.username === userId
-  );
+  const user = userModel.find();
   if (!user) {
     throw new Error("User not found.");
   }
