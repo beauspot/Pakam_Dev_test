ee158c29355bd07a9963e10466be32f7f33390e8
beauspot <beauspot@outlook.com>
2023-07-09 03:53:12 +0100

    altered the wallet services

--- src/api/services/walletServices.js
+++ src/api/services/walletServices.js
@@ -1,12 +1,24 @@
-const axios = require("axios");
+const generateDummyUsers = require("../dummyData");
+
+let users = generateDummyUsers();
 
 // Check wallet balance using User Wallet microservice
 const checkWalletBalance = async (userId, amount) => {
   // Assume an API endpoint '/wallets/:userId/balance' that retrieves wallet balance
-  const response = await axios.get(
-    `https://example.com/wallets/${userId}/balance`
+  // For the sake of this example, we will use the dummy user data to get the wallet balance
+
+  // Retrieve the user from the dummy data
+  const user = users.find(
+    (user) => user.id === userId || user.username === userId
   );
-  const balance = response.data.balance;
+
+  if (!user) {
+    throw new Error("User not found.");
+  }
+
+  // Get the wallet balance from the user object
+  const balance = user.walletBalance;
+
   return balance >= amount;
 };
 
