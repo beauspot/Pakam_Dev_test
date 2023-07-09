21e49c4b261528130086e1e18079e6e71d78b411
beauspot <beauspot@outlook.com>
2023-07-09 13:02:00 +0100

    slightly modified the notification service

--- src/api/services/notificationService.js
+++ src/api/services/notificationService.js
@@ -1,21 +1,26 @@
 require("dotenv").config();
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // Send mobile notification
 const sendMobileNotification = async (mobileNumber, amount) => {
   /// Implementing the logic to send a mobile notification using a third-party SMS service
   // Here, I am using the Twilio SMS API as an example
 
-  const client = require("twilio")(accountSid, authToken);
-
   const accountSid = process.env.TWILLO_ACCOUNT_SID;
   const authToken = process.env.TWILLO_AUTH_TOKEN;
   const twilioPhoneNumber = process.env.TWILLO_PHONE_NUMBER;
+
+  const client = require("twilio")(accountSid, authToken);
+
+  // console.log(mobileNumber);
 
   const msg = await client.messages.create({
     body: `Insufficient funds for deposit of ${amount}`,
     from: twilioPhoneNumber,
     to: mobileNumber,
   });
+  console.log(msg);
   if (!msg) {
     throw new Error("Failed to send mobile notification");
   }
@@ -31,29 +36,24 @@ const sendEmailNotification = async (email, amount) => {
   // we may use a third-party email service or any other notification service.
   // I am using sendGrid for this functionality
 
-  const api_key = proces.env.SENDGRID_API_KEY;
-  const senderMail = process.env.SENDGRID_EMAIL;
+  const mail = {
+    to: email,
+    from: process.env.SENDGRID_EMAIL,
+    subject: "Insuffient Funds in Wallet",
+    text: `your curent funds of ${amount} is insufient for depositing your wallet`,
+    html: "<strong>Your Balance is insuficient to carry out the transfer, Please credit to make transfer</strong>",
+  };
 
-  const mail = await axios.post(
-    "https://api.sendgrid.com/v3/mail/send",
-    {
-      personalizations: [{ to: [{ email }] }],
-      from: { email: senderMail },
-      subject: "Automated Deposit Failure",
-      content: [
-        {
-          type: "text/plain",
-          value: `Insufficient funds for deposit of ${amount}`,
-        },
-      ],
-    },
-    {
-      headers: { Authorization: `Bearer ${api_key}` },
+  try {
+    const response = await sgMail.send(mail);
+    if (sentMail) {
+      return response[0].statusCode.headers;
     }
-  );
+  } catch (error) {
+    return error.message;
+  }
 
   if (!mail) throw new Error("Failed to send email notification");
-
   console.log(
     `Sending email notification to ${email}: Insufficient funds for deposit of ${amount}`
   );
