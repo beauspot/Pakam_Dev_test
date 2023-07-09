47d4f48189ad461aef38997325a39718e5ea284b
beauspot <beauspot@outlook.com>
2023-07-09 03:20:39 +0100

    slightly modified the errHandlerMiddleware.js file.

--- src/api/middleware/errHandlerMiddleware.js
+++ src/api/middleware/errHandlerMiddleware.js
@@ -4,9 +4,6 @@ const { StatusCodes } = require("http-status-codes");
 const errMiddleware = (err, req, res, next) => {
   console.error("Error:", err.message);
   if (err) return res.status(404).json({ errorMessage: err.message });
-  return res
-    .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .json({ errorMessage: err.message });
 };
 
 module.exports = { errMiddleware };
